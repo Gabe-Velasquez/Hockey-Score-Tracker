b969abbcee131f5e4e8b6486d708ad5c1c034aff
Gabe <gabriel.velasquez0116@gmail.com>
2023-11-11 16:03:35 -0800

    styling

--- Hockey-Scores/src/Pages/Home.jsx
+++ Hockey-Scores/src/Pages/Home.jsx
@@ -2,7 +2,6 @@ export default function Home(){
     return(
         <div>
             <h1>Home</h1>
-            <p>Welcome to the home page! This is a simple example site built with Next.js.</p>
             <p><a href="/about">Go to About Page</a></p>
         </div>
     )
