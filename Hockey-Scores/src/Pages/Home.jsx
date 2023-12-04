6fdd185110378998d38d0de132fc989b6a488342
Gabe <gabriel.velasquez0116@gmail.com>
2023-12-03 18:50:10 -0800

    set link for bootstrap on index and made card on home page

--- Hockey-Scores/src/Pages/Home.jsx
+++ Hockey-Scores/src/Pages/Home.jsx
@@ -1,8 +1,26 @@
-export default function Home(){
+import Card  from 'react-bootstrap/Card';
+
+function Home(){
     return(
-        <div className="card">
-            <h1>Home</h1>
-            <p><a href="/about">Go to About Page</a></p>
-        </div>
+        <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+        // <div className="card">
+        //     <h1>Home</h1>
+        //     <p><a href="/about">Go to About Page</a></p>
+        //     <p><a href="/contacts">Go to Contacts Page</a></p>
+        //     <p><a href="/scores">Go to Scores!</a></p>
+        // </div>
     )
 }
+
+export default Home;
