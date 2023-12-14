8bd99ad778f9b5217a4b1efbb5263a967ae46320
Gabe <gabriel.velasquez0116@gmail.com>
2023-12-13 16:43:15 -0800

    working on CSS sheet and having team colors for each team class

--- Hockey-Scores/src/Pages/About.jsx
+++ Hockey-Scores/src/Pages/About.jsx
@@ -6,15 +6,13 @@ export default function About(){
           <Card.Title>About</Card.Title>
           {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
           <Card.Text>
-          This is a simple web application built using React.js, Node.js and Express.js. This application is built to fetch scores, learn about teams (fun facts and trivia), and betting odds of live games.
+          This is a simple web application built using React.js, Node.js and Express.js. This application is built to fetch scores, learn about teams (fun facts and trivia), and betting odds of live games. 
           </Card.Text>
-          
+
+          <Card.Text>
+          I used The Odds API to fetch sport data of upcoming and scores.
+          </Card.Text>
         </Card.Body>
       </Card>
-        // <div className="card">
-        //     <h1 className="m-auto text-center">About</h1>
-        //     <p></p>
-        //     <p>This application is built to fetch scores, learn about teams (fun facts and trivia), and betting odds of live games.</p>
-        // </div>
     )
 }
--- Hockey-Scores/src/Pages/Teams/Avs.jsx
+++ Hockey-Scores/src/Pages/Teams/Avs.jsx
@@ -3,16 +3,16 @@ export default function Avs(){
     // And now the Rocky Mountain Extremes.... nvm Colorado Avalanche
     return(
         <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>Welcome AVALANCHE FANS!!! </Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the content.
-          </Card.Text>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-      </Card>
+          <Card.Body>
+            <Card.Title>Welcome AVALANCHE FANS!!! </Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the content.
+            </Card.Text>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
     )
 }
