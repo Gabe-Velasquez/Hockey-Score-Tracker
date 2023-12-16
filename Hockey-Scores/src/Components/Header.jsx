ebbf28036fccd2177bcc3e5aed914c5573ed4dad
Gabe <gabriel.velasquez0116@gmail.com>
2023-12-16 13:44:19 -0800

    messing around with cards on team pages to best display information. more wireframing to come

--- Hockey-Scores/src/Components/Header.jsx
+++ Hockey-Scores/src/Components/Header.jsx
@@ -12,6 +12,7 @@ export default function Header(){
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
+              <Nav.Link href='/'>Home</Nav.Link>
               <Nav.Link href="/about">About</Nav.Link>
               <Nav.Link href="/scores">Scores</Nav.Link>
               <Nav.Link href='/upcoming'>Upcoming</Nav.Link>
@@ -20,9 +21,6 @@ export default function Header(){
             </Nav>
             <Nav>
               <Nav.Link href="/Contact">Contact</Nav.Link>
-              {/* <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
--- Hockey-Scores/src/Pages/Home.jsx
+++ Hockey-Scores/src/Pages/Home.jsx
@@ -1,31 +1,24 @@
+import Card from 'react-bootstrap/Card';
 
-import Card  from 'react-bootstrap/Card';
-
-
-function Home(){
-    return(
-      <div>
-        
-        <Card style={{ width: '18rem' }}>
+function Home() {
+  return (
+    <div>
+      <Card style={{ width: '18rem' }}>
         <Card.Body>
           <Card.Title>Welcome </Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+          <Card.Subtitle className='mb-2 text-muted'>
+            Card Subtitle
+          </Card.Subtitle>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the content.
           </Card.Text>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
+          <Card.Link href='#'>Card Link</Card.Link>
+          <Card.Link href='#'>Another Link</Card.Link>
         </Card.Body>
-        </Card>
-        </div>
-        // <div className="card">
-        //     <h1>Home</h1>
-        //     <p><a href="/about">Go to About Page</a></p>
-        //     <p><a href="/contacts">Go to Contacts Page</a></p>
-        //     <p><a href="/scores">Go to Scores!</a></p>
-        // </div>
-    )
+      </Card>
+    </div>
+  );
 }
 
 export default Home;
