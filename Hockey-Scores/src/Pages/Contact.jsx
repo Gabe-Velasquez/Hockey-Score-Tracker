d97a55366b5ad487bdca63f01aef061ea66716a8
Gabe <gabriel.velasquez0116@gmail.com>
2023-12-20 09:29:09 -0800

    added email to ways to connect

--- Hockey-Scores/src/Pages/Contact.jsx
+++ Hockey-Scores/src/Pages/Contact.jsx
@@ -1,4 +1,5 @@
 import Card  from 'react-bootstrap/Card';
+import PersonalSocials from '../utils/personalSocials.jsx';
 
 export default function Contact(){
     return(
@@ -9,16 +10,8 @@ export default function Contact(){
           <Card.Text>
           Thank you for using my application! If you have any questions or experience any issues, you can reach out to me on the following platforms: 
           </Card.Text>
-          
+          <PersonalSocials /> 
         </Card.Body>
       </Card>
-        // <div className="card ">
-        //     <h1>Contact</h1>
-        //     <p>You can reach out to me on the following platforms: </p>
-        //     <ul>
-        //         <li><a href="https://www.linkedin.com/in/gabriel-velasquez-/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-        //         <li><a href="https://github.com/Gabe-Velasquez" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-        //     </ul>
-        // </div>
     )
 }
--- Hockey-Scores/src/utils/personalSocials.jsx
+++ Hockey-Scores/src/utils/personalSocials.jsx
@@ -1,15 +1,28 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Card from 'react-bootstrap/Card';
-import {
-  faLinkedin,
-  faXTwitter,
-  faGithub
-} from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-export default function personalSocials(){
-    return(
-        <div>
-        <Card.Link href="https://www.linkedin.com/in/gabriel-velasquez-/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></Card.Link>
-        <Card.Link href="https://github.com/Gabe-Velasquez" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></Card.Link>
-        </div>
-)}
+export default function personalSocials() {
+  return (
+    <div>
+      <Card.Link
+        href='https://www.linkedin.com/in/gabriel-velasquez-/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <FontAwesomeIcon icon={faLinkedin} />
+      </Card.Link>
+      <Card.Link
+        href='https://github.com/Gabe-Velasquez'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <FontAwesomeIcon icon={faGithub} />
+      </Card.Link>
+      <Card.Link href='mailto: gabriel.velasquez0116@gmail.com'>
+        <FontAwesomeIcon icon={faEnvelope} />
+      </Card.Link>
+    </div>
+  );
+}
