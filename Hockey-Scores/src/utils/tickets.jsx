3fbab6f08377e2cb978ff3f23f4b732d011993bb
Gabe <gabriel.velasquez0116@gmail.com>
2024-01-18 18:23:50 -0800

    using seatgeek api and building that component now

---
+++ Hockey-Scores/src/utils/tickets.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const Tickets = ({ team }) => {
+  const API_KEY = import.meta.env.TICKET_API_KEY;
+  const [tickets, setTickets] = useState([]);
+  useEffect(() => {
+    const baseURL = 'https://api.seatgeek.com/2/events';
+    const dynamicURL = `${baseURL}?performers.slug=${encodeURIComponent(
+      team.team
+    )}&client_id=${API_KEY}`;
+
+    axios
+      .get(dynamicURL)
+      .then((res) => {
+        setTickets(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [team.team, API_KEY]);
+  
+  return (
+  <body className='text-center'>
+
+  </body>
+  );
+};
+
+export default Tickets;
