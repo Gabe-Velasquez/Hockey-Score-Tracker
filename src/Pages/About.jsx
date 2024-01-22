37f4447b6dcca4d0587bf6740a666dfe353cf3cb
Gabe <gabriel.velasquez0116@gmail.com>
2024-01-21 17:53:50 -0800

    styling homeand about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,7 +2,7 @@ import Card from 'react-bootstrap/Card';
 export default function About() {
   return (
     <div className='puck'>
-    <Card className='m-auto text-center' style={{ width: '18rem' }}>
+    <Card className='text-center ' style={{marginLeft: 180, marginRight: 180}}>
       <Card.Body>
         <Card.Title>About</Card.Title>
         {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
@@ -11,12 +11,26 @@ export default function About() {
           Express.js. This application is built to fetch scores, learn about
           teams (fun facts and trivia), and betting odds of live games.
         </Card.Text>
-
-        <Card.Text>
-          I used The Odds API to fetch sport data of upcoming and scores.
-        </Card.Text>
-      </Card.Body>
+        </Card.Body>
     </Card>
+      <div className='About-Tiles'>
+        <Card className='puck-card'>
+            <Card.Text>
+              I used The Odds API to fetch sport data of upcoming and scores. In the future, I will display winning odds of each game that is set to play. 
+            </Card.Text>
+        </Card>
+
+        <Card className='puck-card'>
+            <Card.Text>
+              For the ticketing information, I integrated Seat Geek&apos;s API to retrieve data from which team is playing, where and what day. You can even use links generated to purchase tickets from Seat Geek. 
+            </Card.Text>
+        </Card>
+        <Card className='puck-card'>
+            <Card.Text>
+              For the players on each team page, I made an API that will call depending on which team is being loaded. From there, I have it linked to Wikipedia pages for each player that is called. If you wanted to follow a specific team&apos;s social media, I have them listed on each of their pages! 
+            </Card.Text>
+        </Card>
+      </div>
     </div>
   );
 }
