16be2577c297b5605ff482d027b197ebbcd4b7c4
Gabe <gabriel.velasquez0116@gmail.com>
2024-02-02 17:45:24 -0800

    adding google map iframe to put on each team page

---
+++ src/Components/arena.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+const Arena = ({team}) => {
+
+return(
+    <iframe
+  width="600"
+  height="450"
+  style="border:0"
+  loading="lazy"
+  allowfullscreen
+  referrerPolicy="no-referrer-when-downgrade"
+  src="https://www.google.com/maps/embed/v1/place?key=
+    &q=Space+Needle,Seattle+WA">
+</iframe>
+)
+}
+
+export default Arena
