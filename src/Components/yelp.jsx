5f838ee77040f1e1b841c70b29231c9decfb0f74
Gabe <gabriel.velasquez0116@gmail.com>
2024-02-08 10:26:21 -0800

    completed component and need to place it in teams page

--- src/Components/yelp.jsx
+++ src/Components/yelp.jsx
@@ -6,35 +6,42 @@ const Yelp = ({ team }) => {
   const API_KEY = import.meta.env.VITE_YELP_KEY;
   const [restaurants, setRestaurants] = useState([]);
   useEffect(() => {
-    const baseURL = 'https://api.yelp.com/v3/businesses/search';
-    const teamSlug = encodeURIComponent(team.team.replace(/\s+/g, '-'));
-    const dynamicURL = `${baseURL}?performers.slug=${teamSlug}&client_id=${API_KEY}`;
+    const baseURL = 'https://api.yelp.com/v3/businesses/search?';
+    const addressSlug = encodeURIComponent(
+      team.arenaAddress.replace(/\s+/g, '-')
+    );
+    const dynamicURL = `${baseURL}location=${addressSlug}&radius=5000&&limit=3&client_id=${API_KEY}`;
     console.log(dynamicURL);
     axios
-      .get(dynamicURL)
+      .get(dynamicURL, {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+        },
+      })
       .then((res) => {
-        console.log(res.data)
+        console.log(res.data);
         setRestaurants(res.data.events);
       })
       .catch((err) => console.log(err));
-  }, [team.team, API_KEY]) ;
-  
+  }, [team.arenaAddress, API_KEY]);
+
   return (
     <div className='text-center'>
-      
       <h4>Food Nearby</h4>
       {restaurants.length > 0 ? (
-        <div className="row justify-content-center">
-        <ul>
-          {restaurants.slice(0, 5).map((event) => (
-            <li key={event.id}>
-              {/* Renders a clickable link for each event */}
-              <a href={event.url} >
-                {event.title}
-              </a>
-            </li>
-          ))}
-        </ul>
+        <div className='row justify-content-center'>
+          <ul>
+            {restaurants.slice(0, 5).map((restaurant) => (
+              <li key={restaurant.id}>
+                {/* Renders a clickable link for each restaurant */}
+                <a href={restaurant.url}>
+                  <img src={restaurant.image_url} alt={restaurant.name} />
+                  {restaurant.name}
+                  {restaurant.rating} ⭐{restaurant.price}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       ) : (
         <p>Nothing found nearby... hopefully the arena has good food.</p>
@@ -45,7 +52,7 @@ const Yelp = ({ team }) => {
 
 Yelp.propTypes = {
   team: PropTypes.shape({
-    team: PropTypes.string.isRequired,
+    arenaAddress: PropTypes.string.isRequired,
   }).isRequired,
 };
 
