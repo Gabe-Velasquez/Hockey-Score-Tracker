4cb157128ff450c429d3ec7b1c3c383416ba5742
Gabe <gabriel.velasquez0116@gmail.com>
2024-02-15 10:45:42 -0800

    still getting errors on yelp. researching more. Changed tickets component

--- src/Components/yelp.jsx
+++ src/Components/yelp.jsx
@@ -1,16 +1,77 @@
+// import axios from 'axios';
+// import PropTypes from 'prop-types';
+// import { useEffect, useState } from 'react';
+
+// const Yelp = ({ team }) => {
+//   const API_KEY = import.meta.env.VITE_YELP_KEY;
+//   const [restaurants, setRestaurants] = useState([]);
+//   useEffect(() => {
+//     const baseURL = 'https://api.yelp.com/v3/businesses/search?';
+//     const addressSlug = encodeURIComponent(
+//       team.arenaAddress.replace(/\s+/g, '-')
+//     );
+//     const dynamicURL = `${baseURL}location=${addressSlug}&radius=5000&&limit=3&client_id=${API_KEY}`;
+//     console.log(dynamicURL);
+//     axios
+//       .get(dynamicURL, {
+//         headers: {
+//           Authorization: `Bearer ${API_KEY}`,
+//         },
+//       })
+//       .then((res) => {
+//         console.log(res.data);
+//         setRestaurants(res.data.events);
+//       })
+//       .catch((err) => console.log(err));
+//   }, [team.arenaAddress, API_KEY]);
+
+//   return (
+//     <div className='text-center'>
+//       <h4>Food Nearby</h4>
+//       {restaurants.length > 0 ? (
+//         <div className='row justify-content-center'>
+//           <ul>
+//             {restaurants.slice(0, 5).map((restaurant) => (
+//               <li key={restaurant.id}>
+//                 {/* Renders a clickable link for each restaurant */}
+//                 <a href={restaurant.url}>
+//                   <img src={restaurant.image_url} alt={restaurant.name} />
+//                   {restaurant.name}
+//                   {restaurant.rating} ⭐{restaurant.price}
+//                 </a>
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//       ) : (
+//         <p>Nothing found nearby... hopefully the arena has good food.</p>
+//       )}
+//     </div>
+//   );
+// };
+
+// Yelp.propTypes = {
+//   team: PropTypes.shape({
+//     arenaAddress: PropTypes.string.isRequired,
+//   }).isRequired,
+// };
+
+// export default Yelp;
+
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 
 const Yelp = ({ team }) => {
   const API_KEY = import.meta.env.VITE_YELP_KEY;
+  console.log(`API Key: ${API_KEY}`);
   const [restaurants, setRestaurants] = useState([]);
   useEffect(() => {
     const baseURL = 'https://api.yelp.com/v3/businesses/search?';
     const addressSlug = encodeURIComponent(
       team.arenaAddress.replace(/\s+/g, '-')
     );
-    const dynamicURL = `${baseURL}location=${addressSlug}&radius=5000&&limit=3&client_id=${API_KEY}`;
+    const dynamicURL = `${baseURL}location=${addressSlug}&radius=5000&limit=3`;
     console.log(dynamicURL);
     axios
       .get(dynamicURL, {
@@ -20,7 +81,7 @@ const Yelp = ({ team }) => {
       })
       .then((res) => {
         console.log(res.data);
-        setRestaurants(res.data.events);
+        setRestaurants(res.data.businesses);
       })
       .catch((err) => console.log(err));
   }, [team.arenaAddress, API_KEY]);
@@ -31,7 +92,7 @@ const Yelp = ({ team }) => {
       {restaurants.length > 0 ? (
         <div className='row justify-content-center'>
           <ul>
-            {restaurants.slice(0, 5).map((restaurant) => (
+            {restaurants.map((restaurant) => (
               <li key={restaurant.id}>
                 {/* Renders a clickable link for each restaurant */}
                 <a href={restaurant.url}>
