e9114a2763f601fff37c3fe3e3d3106a1e41e641
Gabe <gabriel.velasquez0116@gmail.com>
2024-02-16 06:42:46 -0800

    adding condition render on upcoming page

--- src/Pages/Upcoming.jsx
+++ src/Pages/Upcoming.jsx
@@ -8,7 +8,7 @@ export default function Upcoming(){
       axios.get(`https://api.the-odds-api.com/v4/sports/icehockey_nhl/scores/?completed=false&apiKey=${API_KEY}`)
       .then(res=>{setScores(res.data)})
       .catch(err=>console.log(err))
-    })
+    }, [])
       return(
         // going to add more to this page. 
         <div>
