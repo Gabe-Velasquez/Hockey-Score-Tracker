160165e92942c04460684143e11b7e63f3338e2f
Gabe <gabriel.velasquez0116@gmail.com>
2024-04-23 17:35:23 -0700

    added hockey score component that way I can have scores on the home page for playoff purposes.

---
+++ src/Components/scores.jsx
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+export default function Scores(){
+  const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
+  const [scores, setScores] = useState([]);
+  useEffect(()=>{
+    axios.get(`https://api.the-odds-api.com/v4/sports/icehockey_nhl/scores/?daysFrom=1&apiKey=${API_KEY}`)
+    .then(res=>{setScores(res.data)})
+    .catch(err=>console.log(err))
+}, []) 
+
+    return(
+      <div>
+        <h3 className='text-center'>Current Score</h3>
+      <table className='centered-t text-center'>
+          <thead>
+            <tr>
+              <th>Home Team</th>
+              <th>Home Score</th>
+              <th>Away Team</th>
+              <th>Away Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {scores && scores.map((r,i)=>(
+              r.scores !== null && r.completed === false &&
+              <tr key={i}>
+                <td>{r.home_team}</td>
+                <td>{r.scores && r.scores.find(team => team.name === r.home_team)?.score}</td>
+                <td>{r.away_team}</td>
+                <td>{r.scores && r.scores.find(team => team.name === r.away_team)?.score}</td>
+              </tr>
+              ))
+            }
+          </tbody>
+        </table>
+        <hr></hr>
+      <h3 className='text-center'>Concluded Games</h3>
+      <table className='centered-t text-center'>
+          <thead>
+            <tr>
+              <th>Home Team</th>
+              <th>Home Score</th>
+              <th>Away Team</th>
+              <th>Away Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {scores && scores.map((r,i)=>(
+              r.scores !== null && r.completed === true &&
+              <tr key={i}>
+                <td>{r.home_team}</td>
+                <td>{r.scores && r.scores.find(team => team.name === r.home_team)?.score}</td>
+                <td>{r.away_team}</td>
+                <td>{r.scores && r.scores.find(team => team.name === r.away_team)?.score}</td>
+              </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,24 @@
 import Card from 'react-bootstrap/Card';
+import Scores from '../components/Scores.jsx';
+
 
 function Home() {
   return (
     <div className='puck'>
-      <Card className='m-auto text-center puck-card' style={{ width: '18rem', display:'flex', justifyContent:'center', alignItems:'center', }}>
+      <Card className='text-center puck-card' style={{ width: '18rem', display:'flex', justifyContent:'center', alignItems:'center', }}>
         <Card.Body>
           <Card.Title>Welcome to my NHL Score Tracker Application</Card.Title>
-          <Card.Subtitle className='mb-2 text-muted'>
-            
-          </Card.Subtitle>
+          
           <Card.Text>
             This project started out as a way to get NHL easier than the official nhl app. With this application, you can see scores of live games while checking out any upcoming hockey games. Thinking of going to a game? Check out a teams page and you can see the next 5 games for your favorite team!  
           </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className='text-center puck-card' style={{ width: '18rem', display:'flex', justifyContent:'center', alignItems:'center', }}>
+        <Card.Body>
+          <h1>Playoff Hockey is HERE!</h1>
+
+          <Scores />
         </Card.Body>
       </Card>
     </div>
